a50ad71d91d49b71ec81eb481097bf522b0136fe
Umair Ahmad <70979817+umair0228@users.noreply.github.com>
2024-03-05 23:24:51 +0000

    Resolved Issue#4 to add user age to helloWorld.py
    User age input added to helloWorld.py for printing with name and message

--- helloWorld.py
+++ helloWorld.py
@@ -1,6 +1,11 @@
 # This is a program written in Python.
-# The program first takes an input string message from the user.
-# Then the program prints the string message and ends.
+# The program takes the user name and age as input.
+# The program takes an input string message from the user.
+# Then the program prints user's name, age and the string message before ending.
 
+user_name = input("Please enter your name: ")
+user_age = input("Please enter your age: ")
 message = input("Please enter your text message: ")
-print(message)
+print(f"User name is: {user_name}")
+print(f"User age is: {user_age}")
+print(f"User message is: {message}")
