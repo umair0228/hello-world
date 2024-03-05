0da2b35a0a258391d345ea3697ec671e18da9278
Umair Ahmad <70979817+umair0228@users.noreply.github.com>
2024-03-05 22:43:32 +0000

    Add files via upload
    helloWorld.py added by Umair

---
+++ helloWorld.py
@@ -0,0 +1,6 @@
+# This is a program written in Python.
+# The program first takes an input string message from the user.
+# Then the program prints the string message and ends.
+
+message = input("Please enter your text message: ")
+print(message)
